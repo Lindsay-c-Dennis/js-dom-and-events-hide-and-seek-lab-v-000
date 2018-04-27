10da630fcbce9da424f709628aecfdcf61443f5b
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-27 15:27:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,13 +19,16 @@ function deepestChild() {
 }
 
 function increaseRankBy(n) {
- const rankedLists = document.querySelectorAll('.ranked-list');
-
-  for (let i = 0, l = rankedLists.length; i < l; i++) {
-    let children = rankedLists[i].children;
-
-    for (let j = 0, k = children.length; j < k; j++) {
-      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
-    }
-  }
+  const rankings = document.querySelectorAll('ranked-list');
+ 
+  let children = [];
+ 
+  for (let i=0, h = rankings.length; i < h; i++) {
+    children.push(rankings[i].children);
+ 
+  for (let j=0, k = children.length; j < k; j++) {
+    children[j].innerHTML = (parseInt(children[j].innerHTML) + n);
+  }}
+ 
+ 
 }
