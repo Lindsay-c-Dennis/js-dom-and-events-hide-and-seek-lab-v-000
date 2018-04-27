1ba64e4e5280975761e1bf7744e295db92ddf3ea
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-27 15:17:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function increaseRankBy(n) {
  const rankings = document.querySelectorAll('ranked-list');
  
  for (let i=0; i < rankings.length; i++) {
-   let children = ranking[i].children;
+   let children = rankings[i].children;
  
  for (let j=0; j < children.length; j++) {
    children[j].innerHTML = parseInt(children[j].innerHTML) + n;
